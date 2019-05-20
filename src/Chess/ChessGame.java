e4b0573476836442716213f882844e7507025490
winstongergill <winstongergill612@gmail.com>
2019-05-19 19:01:38 -0700

    made initial board copy method, still WIP

--- src/Chess/ChessGame.java
+++ src/Chess/ChessGame.java
@@ -1,10 +1,13 @@
 package Chess;
 
 import externalBoard.BoardSetup;
+import internalBoard.Board;
 
 public class ChessGame {
 
     public static void main(String[] args) {
-        new BoardSetup();
+       BoardSetup boardSetup =  new BoardSetup();
+        Board board = new Board(boardSetup.board);
+        System.out.println("");
     }
 }
